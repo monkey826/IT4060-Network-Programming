f48c3556d5be7845b642dbd53bbdf4969efcd9e6
monkey826 <portgas.d.ace995@gmail.com>
2016-01-29 21:54:31 +0700

    UDP-DNS OK

--- UDP-DNS/Server/Server.cpp
+++ UDP-DNS/Server/Server.cpp
@@ -1,5 +1,5 @@
 
-// Author : monkey
+
 #include "stdafx.h"
 #include "conio.h"
 #include "stdio.h"
@@ -7,15 +7,15 @@
 #include "ws2tcpip.h"
 #define BUFF_SIZE 2048
 int main(int argc, char* argv[]) {
-	// Step 1: Initiate winsock
+	// Step 1: Inittiate WinSock
 	WSADATA wsaData;
 	WORD wVersion = MAKEWORD(2, 2);
 	if (WSAStartup(wVersion, &wsaData))
 		printf(" Version is not supported \n ");
-	// Step 2: Construct socket
+	// Step 2: Construct socket	
 	SOCKET server;
 	server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	// Step 3: bind address to socket
+	//Step 3: Bind address to socket
 	sockaddr_in serverAddr;
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(atoi(argv[1]));
@@ -27,13 +27,12 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 	printf(" Server Started! \n ");
-	// Step 4: communicate with client
+	// Step 4: Communicate with client
 	sockaddr_in clientAddr, resultAddr;
 	char buff[BUFF_SIZE], msg[BUFF_SIZE];
 	int ret, clientAddrLen = sizeof(clientAddr);
-	addrinfo *pHead = NULL; // using pointer type of struct addrinfo
+	addrinfo *pHead = NULL; // pointer type of struct addrinfo
 	while (1) {
-		// Receive message
 		ret = recvfrom(server, buff, BUFF_SIZE, 0, (sockaddr *)&clientAddr, &clientAddrLen);
 		if (ret == SOCKET_ERROR)
 			printf(" Error: %d", WSAGetLastError());
@@ -41,30 +40,26 @@ int main(int argc, char* argv[]) {
 		{
 			buff[ret] = 0;
 			printf(" Receive from client [%s:%d] %s \n",
-				inet_ntoa(clientAddr.sin_addr), htons(clientAddr.sin_port), buff);
-			// Get IP address from domain
-			ret = getaddrinfo(buff, "http", NULL, &pHead);//Get name -> IP -> pHead pointer
+				inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), buff);
+			ret = getaddrinfo(buff, "http", NULL, &pHead); // Convert hostname to ip and give it to pHead pointer
 			if (ret != 0) {
 				sprintf(msg, "Server: Resolve failed %d ! \n", ret);
-				// echo to client
-				sendto(server, msg, strlen(msg), 0, (sockaddr *)&clientAddr, sizeof(clientAddr));
+				sendto(server, msg, strlen(msg), 0, (SOCKADDR *)&clientAddr, sizeof(clientAddr));
 			}
 			else {
-				while (pHead != NULL) { // Loop pointer
+				while (pHead != NULL) {
 					memcpy(&resultAddr, pHead->ai_addr, pHead->ai_addrlen); // Cpy addr -> resultAddr.
 					sprintf(msg, "Server: %s:%d \n", inet_ntoa(resultAddr.sin_addr), htons(resultAddr.sin_port));
-					 //echo to client 
-					sendto(server, msg, strlen(msg), 0, (sockaddr *)&clientAddr,
+					sendto(server, msg, strlen(msg), 0, (sockaddr*)&clientAddr,
 						sizeof(clientAddr));
-					pHead = pHead->ai_next;// Pointer next for next domain
-				} // end while
+					pHead = pHead->ai_next; // Next host name
+				}
 			}
 		}
-	}// end while
-	 // Step 5: Close socket
+	}
+	// Step 5: close socket
 	closesocket(server);
-	// Step 6: Terminate Winsock
+	// Step 6: terminate winsock 
 	WSACleanup();
-
 	return 0;
 }
